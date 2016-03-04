78b77d10a5a9b363806036451122e45f04ec13ab
Steve Lydford <stevelydford@gmail.com>
2016-03-04 11:38:04 +0000

    Initial Commit

---
+++ BowlingGame.Tests/GameShould.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+
+namespace BowlingGame.Tests
+{
+    [TestFixture]
+    public class GameShould
+    {
+        private Game _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+
+        [Test]
+        public void ScoreZeroForAGutterGame()
+        {
+            RollMany(20, 0);
+
+            Assert.That(_game.Score(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ScoreTwentyIfRolledAllOnes()
+        {
+            RollMany(20, 1);
+
+            Assert.That(_game.Score(), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void AwardASpareForKnockingDownTenPinsInASingleFrame()
+        {
+            RollSpare();
+            _game.Roll(1);
+            RollMany(17, 0);
+
+            Assert.That(_game.Score(), Is.EqualTo(12));
+        }
+
+        [Test]
+        public void AwardAStrikeForKnockingDownTenPinsOnFirstBallOfFrame()
+        {
+            RollStrike();
+            _game.Roll(1);
+            _game.Roll(1);
+            RollMany(16, 0);
+
+            Assert.That(_game.Score(), Is.EqualTo(14));
+        }
+
+        [Test]
+        public void GiveCorrectScoreForAPerfectGame()
+        {
+            RollMany(12, 10);
+            
+            Assert.That(_game.Score(), Is.EqualTo(300));
+        }
+
+        private void RollStrike()
+        {
+            _game.Roll(10);
+        }
+
+        private void RollSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (var i = 0; i < rolls; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
+    }
+}
---
+++ BowlingGame/Game.cs
@@ -0,0 +1,50 @@
+﻿namespace BowlingGame
+{
+    public class Game
+    {
+        private readonly int[] _rolls = new int[21];
+        private int _currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            _rolls[_currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            var score = 0;
+            var frameIndex = 0;
+
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + _rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += _rolls[frameIndex] + _rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return _rolls[frameIndex] == 10;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+        }
+    }
+}
